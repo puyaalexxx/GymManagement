eb8f4c793b9dcbf2fcca813ae79f7a61eb03dbf8
Alex <puyaalexxx@gmail.com>
2025-06-23 14:01:00 +0300

    added the first controller

---
+++ src/GymManagement.Api/Controllers/SubscriptionsController.cs
@@ -0,0 +1,22 @@
+﻿using GymManagement.Contracts.Subscriptions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GymManagement.Api.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class SubscriptionsController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult CreateSubscription(CreateSubscriptionRequest request)
+        {
+            return Ok(request);
+        }
+
+        [HttpGet]
+        public IActionResult GetSubscription()
+        {
+            return Ok("success");
+        }
+    }
+}
